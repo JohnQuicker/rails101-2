6bb9ba56d651a9c60dfacc4cc55727b48aab54cb
John Quicker <johnquicker@163.com>
2017-04-06 20:24:02 +0800

    implement groups#new

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,14 @@
 class GroupsController < ApplicationController
+# Define the talk groups indexing function, which the main page shows.
   def index
     @groups = Group.all
   end
+
+# Define the new action, which is a model(group)'s method.
+  def new
+    @group = Group.new
+  end
+
+
+
 end
