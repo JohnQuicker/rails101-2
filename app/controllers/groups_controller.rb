d0c45eb620daa5fd3a5cd2c7b2511df2020b22e1
John Quicker <johnquicker@163.com>
2017-04-07 07:34:33 +0800

    implement groups#destroy

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,10 +1,10 @@
 class GroupsController < ApplicationController
+
 # Define the talk groups indexing function, which the main page shows.
   def index
     @groups = Group.all
   end
-#
-#
+
 # Define the new action, which is a model(group)'s method.
   def new
     @group = Group.new
@@ -17,17 +17,29 @@ class GroupsController < ApplicationController
     redirect_to groups_path
   end
 
-private
+# Define the group title click action.
+  def show
+    @group = Group.find(params[:id])
+  end
+
+# Define the group title click action.
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+# Define delete button's action.
+  def destroy
+    @group = Group.find(params[:id])
+    @group.destroy
+    flash[:alert] = "讨论区被删除了"
+    redirect_to groups_path
+  end
+
+  private
 
 # Define the params format, whoes parameters were get from the new_html page.
-def group_params
-  params.require(:group).permit(:title, :description)
-end
-#
-#
-# Define the group title click action.
-def shows
-  @group = Group.find(params[:id])
-end
+  def group_params
+    params.require(:group).permit(:title, :description)
+  end
 
 end
